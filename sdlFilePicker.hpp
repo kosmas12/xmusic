b75aca6a9addfff4f9bade14b71c69cd166d38ff
Kosmas Raptis <keeperkosmas6@gmail.com>
2021-01-18 22:50:25 +0200

    Add ability to change directories on PC

--- sdlFilePicker.hpp
+++ sdlFilePicker.hpp
@@ -28,6 +28,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*
 #endif
 #include <string>
 #include <algorithm>
+#include <iostream>
 #include "lib/FilesystemX/FilesystemX.hpp"
 
 std::string fileToPlay = "";
@@ -64,11 +65,15 @@ void CloseFirstController() {
 
 #if defined(NXDK)
 #define ROOT "D:\\"
+#define SEPARATOR "\\"
 #elif defined (__WIN32__)
 #define ROOT ".\\"
+#define SEPARATOR "\\"
 #else
 #define ROOT "./"
+#define SEPARATOR "/"
 #endif
+std::string curPath = ROOT "Music" SEPARATOR;
 SDL_Event event;
 TTF_Font *Roboto = NULL;
 int curSelection = 0;
@@ -76,9 +81,9 @@ SDL_Color color = {255, 255, 255};
 Mix_Music *music = NULL;
 
 int InitFilePicker() {
-    int ret = TTF_Init();
-    ret += IMG_Init(IMG_INIT_PNG);
-    return ret;
+  int ret = TTF_Init();
+  ret += IMG_Init(IMG_INIT_PNG);
+  return ret;
 }
 
 void Quit(Mix_Music *music, int exitcode) {
@@ -99,106 +104,114 @@ void Quit(Mix_Music *music, int exitcode) {
 }
 
 void Draw(SDL_Surface *borderImage, SDL_Surface *arrowImage, SDL_Window *window, std::vector<ProtoFS::fileEntry> listDir) {
-    SDL_BlitSurface(borderImage, NULL, SDL_GetWindowSurface(window), NULL);
-    pos = {95, 20, 500, 20};
-    for (int i = 0; i < std::min((int)listDir.size(), (int) 20); i++) {
-        text = TTF_RenderText_Blended(Roboto, listDir[i].fileName.c_str(), color);
-        pos.y += 20;
-        SDL_BlitSurface(text, NULL, SDL_GetWindowSurface(window), &pos);
-        SDL_FreeSurface(text);
-        if (i == (curSelection % 20)) {
-            pos.x = 45;
-            SDL_BlitSurface(arrowImage, NULL, SDL_GetWindowSurface(window), &pos);
-            pos.x = 95;
-        }
+  SDL_BlitSurface(borderImage, NULL, SDL_GetWindowSurface(window), NULL);
+  pos = {95, 20, 500, 20};
+  for (int i = 0; i < std::min((int)listDir.size(), (int) 20); i++) {
+    text = TTF_RenderText_Blended(Roboto, listDir[i].fileName.c_str(), color);
+    pos.y += 20;
+    SDL_BlitSurface(text, NULL, SDL_GetWindowSurface(window), &pos);
+    SDL_FreeSurface(text);
+    if (i == (curSelection % 20)) {
+      pos.x = 45;
+      SDL_BlitSurface(arrowImage, NULL, SDL_GetWindowSurface(window), &pos);
+      pos.x = 95;
     }
+  }
 }
 
 int showFilePicker(SDL_Window *window) {
-    SDL_Surface* borderImage = IMG_Load(ROOT"border.png");
-    SDL_Surface* arrowImage = IMG_Load(ROOT"arrow.png");
-    Roboto = TTF_OpenFont(ROOT"Roboto-Regular.ttf", 15);
-    #if defined (NXDK) || defined (_WIN32)
-    ProtoFS::FilesystemX fs(ROOT"Music\\");
-    #else
-    ProtoFS::FilesystemX fs(ROOT"Music/");
-    #endif
-    std::vector<ProtoFS::fileEntry> listDir = fs.listDir();
-    while (true) {
-      if(Roboto != NULL){
-        Draw(borderImage, arrowImage, window, listDir);
-        while (SDL_PollEvent(&event)) {
-          switch(event.type){
-            case SDL_QUIT:
-              exitted = 1;
-              Quit(music, 0);
-            case SDL_KEYDOWN:
-              switch (event.key.keysym.sym) {
-                case SDLK_UP:
-                  if(curSelection > 0) {
-                    curSelection--;
-                  }
-                  else {
-                    curSelection = (int)listDir.size() - 1;
-                  }
-                  break;
-                case SDLK_DOWN:
-                  if(curSelection < (int)listDir.size() - 1) {
-                    curSelection++;
-                  }
-                  else {
-                    curSelection = 0;
-                  }
-                  break;
-                case SDLK_RETURN:
-                  fileToPlay = listDir[curSelection].filePath;
-                  return (int)listDir.size();
-                case SDLK_ESCAPE:
-                  exitted = 1;
-                  Quit(music, 0);
-                default:
-                  break;
+  SDL_Surface* borderImage = IMG_Load(ROOT"border.png");
+  SDL_Surface* arrowImage = IMG_Load(ROOT"arrow.png");
+  Roboto = TTF_OpenFont(ROOT"Roboto-Regular.ttf", 15);
+  ProtoFS::FilesystemX *fs;
+  fs = new ProtoFS::FilesystemX(curPath);
+  std::vector<ProtoFS::fileEntry> listDir = fs->listDir();
+  while (true) {
+    if(Roboto != NULL){
+      Draw(borderImage, arrowImage, window, listDir);
+      while (SDL_PollEvent(&event)) {
+        switch(event.type) {
+          case SDL_QUIT:
+            exitted = 1;
+            Quit(music, 0);
+          case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+              case SDLK_UP:
+                if (curSelection > 0) {
+                  curSelection--;
+                } else {
+                  curSelection = (int) listDir.size() - 1;
                 }
-              break;
-            case SDL_CONTROLLERBUTTONDOWN:
-              switch(event.cbutton.button) {
-                case SDL_CONTROLLER_BUTTON_DPAD_UP:
-                  if(curSelection > 0) {
-                    curSelection--;
-                  }
-                  else {
-                    curSelection = (int)listDir.size() - 1;
-                  }
-                  break;
-                case SDL_CONTROLLER_BUTTON_DPAD_DOWN:
-                  if(curSelection < (int)listDir.size() - 1) {
-                    curSelection++;
-                  }
-                  else {
-                    curSelection = 0;
-                  }
-                  break;
-                case SDL_CONTROLLER_BUTTON_A:
-                  fileToPlay = listDir[curSelection].filePath;
-                  return (int)listDir.size();
-                case SDL_CONTROLLER_BUTTON_B:
-                  exitted = 1;
-                  Quit(music, 0);
-                default:
-                  break;
+                break;
+              case SDLK_DOWN:
+                if (curSelection < (int) listDir.size() - 1) {
+                  curSelection++;
+                } else {
+                  curSelection = 0;
                 }
-              break;
-            default:
-              break;
-          }
+                break;
+              case SDLK_RETURN:
+                if (listDir[curSelection].type == ProtoFS::fileType::Folder) {
+                  curPath = curPath + listDir[curSelection].fileName + SEPARATOR;
+                  delete fs;
+                  fs = new ProtoFS::FilesystemX(curPath);
+                  listDir = fs->listDir();
+                }
+                else {
+                  fileToPlay = listDir[curSelection].filePath;
+                  return (int) listDir.size();
+                }
+                break;
+              case SDLK_ESCAPE:
+                exitted = 1;
+                Quit(music, 0);
+                break;
+              case SDLK_BACKSPACE:
+                curPath = curPath + ".." + SEPARATOR;
+                delete fs;
+                fs = new ProtoFS::FilesystemX(curPath);
+                listDir = fs->listDir();
+                break;
+            }
+            break;
+          case SDL_CONTROLLERBUTTONDOWN:
+            switch (event.cbutton.button) {
+              case SDL_CONTROLLER_BUTTON_DPAD_UP:
+                if (curSelection > 0) {
+                  curSelection--;
+                } else {
+                  curSelection = (int) listDir.size() - 1;
+                }
+                break;
+              case SDL_CONTROLLER_BUTTON_DPAD_DOWN:
+                if (curSelection < (int) listDir.size() - 1) {
+                  curSelection++;
+                } else {
+                  curSelection = 0;
+                }
+                break;
+              case SDL_CONTROLLER_BUTTON_A:
+                fileToPlay = listDir[curSelection].filePath;
+                return (int) listDir.size();
+              case SDL_CONTROLLER_BUTTON_B:
+                exitted = 1;
+                delete fs;
+                Quit(music, 0);
+              default:
+                break;
+            }
+            break;
+          default:
+            break;
         }
-        SDL_UpdateWindowSurface(window);
       }
-      else {
-        printf("Couldn't initialize font. Reason: %s", TTF_GetError());
-        SDL_Delay(200);
-        break;
-      }
+      SDL_UpdateWindowSurface(window);
     }
-    return (int)listDir.size();
+    else {
+      printf("Couldn't initialize font. Reason: %s", TTF_GetError());
+      SDL_Delay(200);
+      break;
+    }
+  }
+  return (int)listDir.size();
 }
